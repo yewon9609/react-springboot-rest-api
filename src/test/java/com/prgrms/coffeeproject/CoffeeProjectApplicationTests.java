8a37a8939c53ac367dfc648f19749085946db3b6
yewon9609 <dodnjs1241@naver.com>
2022-12-01 21:43:03 +0900

    환경설정

---
+++ src/test/java/com/prgrms/coffeeproject/CoffeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.coffeeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
